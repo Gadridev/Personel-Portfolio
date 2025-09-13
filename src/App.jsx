3d935d6cd7db074e5c2de9b2988c870f5acf1005
mohamed gadri <gadrimohamed8@gmail.com>
2025-09-13 13:18:42 +0100

    Remove dark/light mode feature and update GlobalStyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { ThemeProvider as StyledThemeProvider } from 'styled-components';
-import { ThemeProvider, useTheme } from './context/ThemeContext';
-import { GlobalStyles } from './styles/GlobalStyles';
-import { theme as themeConfig } from './styles/theme';
 import AppLayout from './ui/AppLayout';
 import About from './components/about/About';
 import Resume from './components/resume/Resume';
@@ -11,41 +7,9 @@ import Contact from './components/contact/Contact';
 import { ResumeProvider } from './Context/ResumeContext';
 import { Toaster } from 'react-hot-toast';
 
-function ToasterWithTheme() {
-  const { isDark } = useTheme();
-  const currentTheme = isDark ? themeConfig.dark : themeConfig.light;
-
+function App() {
   return (
-    <Toaster
-      position="top-center"
-      gutter={12}
-      containerStyle={{ margin: "8px" }}
-      toastOptions={{
-        success: {
-          duration: 3000,
-        },
-        error: {
-          duration: 5000,
-        },
-        style: {
-          fontSize: "16px",
-          maxWidth: "500px",
-          padding: "16px 24px",
-          backgroundColor: currentTheme.background.card,
-          color: currentTheme.text.primary,
-        },
-      }}
-    />
-  );
-}
-
-function ThemedApp() {
-  const { isDark } = useTheme();
-  const currentTheme = isDark ? themeConfig.dark : themeConfig.light;
-
-  return (
-    <StyledThemeProvider theme={currentTheme}>
-      <GlobalStyles />
+    <>
       <BrowserRouter>
         <ResumeProvider>
           <Routes>
@@ -59,16 +23,21 @@ function ThemedApp() {
           </Routes>
         </ResumeProvider>
       </BrowserRouter>
-      <ToasterWithTheme />
-    </StyledThemeProvider>
-  );
-}
-
-function App() {
-  return (
-    <ThemeProvider>
-      <ThemedApp />
-    </ThemeProvider>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+          },
+        }}
+      />
+    </>
   );
 }
 
